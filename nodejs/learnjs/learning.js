82ca641658b33e75a11c31748aad01d1293cfb29
psnnice <niceosurn@gmail.com>
2023-10-24 15:11:41 +0700

    try

---
+++ nodejs/learnjs/learning.js
@@ -0,0 +1,9 @@
+const name = 'Devahoy'
+
+const hello = name => 'Hello ${name}'
+
+function sum(a, b) {
+  return  a + b
+ }
+
+module.exports.sum = sum
---
+++ nodejs/learnjs/learnmore.js
@@ -0,0 +1,6 @@
+
+const sum = require('./learning.js')
+
+//const resault = sum(1,3)
+
+console.log(sum.sum(1,2))

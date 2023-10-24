b6fe7b86844954bb676d2e4fc114a0a1f5744373
psnnice <niceosurn@gmail.com>
2023-10-24 15:15:41 +0700

    tryna

---
+++ react/src/component/MyCOmponent.tsx
@@ -0,0 +1,15 @@
+function MyCOmponent() {
+  return (
+    <div>
+      <p>this is my companent</p>
+      <p>this is my companent
+      this is my companent
+      this is my companent</p>
+      <p>this is my companent</p>
+      <p>this is my companent</p>
+    </div>
+  )
+}
+
+export default MyCOmponent
+
